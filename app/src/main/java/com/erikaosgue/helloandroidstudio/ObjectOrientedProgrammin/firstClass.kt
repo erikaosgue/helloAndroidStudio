5652b81f55df9d0a39ee74e00258c67b6855013b
erikaosgue <1476@holbertonschool.com>
2021-02-18 15:18:03 -0500

    Add the basics of OOP: Create a class with some porperties and some methods

---
+++ app/src/main/java/com/erikaosgue/helloandroidstudio/ObjectOrientedProgrammin/firstClass.kt
@@ -0,0 +1,41 @@
+package com.erikaosgue.helloandroidstudio.ObjectOrientedProgrammin
+
+class Person {
+
+    //Creating Properties
+    var name: String = "Josh"
+    val age: Int = 42
+
+
+    //Adding Methods
+    fun speak() {
+        println("Hello")
+    }
+    fun greet(name: String) {
+        println("Hello $name")
+    }
+    fun getYearOfBirth(age: Int): Int {
+        return 2016-age
+    }
+
+    fun getYearOfBirth2(age: Int) = 2016 - age
+}
+
+fun main() {
+    val person = Person()
+    println(person.name)
+    println(person.age)
+
+    //We can reassing the value of name because
+    // the variable is not constants
+    // the opposite occures with age, because
+    // it is a constant
+
+    person.name = "Juli"
+    println(person.name)
+
+    person.speak()
+    person.greet("Andrea")
+    println(person.getYearOfBirth(22))
+    println(person.getYearOfBirth2(22))
+}

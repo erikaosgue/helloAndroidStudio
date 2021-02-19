f54f2e01f06cc8b9f74ca9a9eff10258c6f0dc8d
erikaosgue <1476@holbertonschool.com>
2021-02-19 09:18:01 -0500

    Add challenge class

--- app/src/main/java/com/erikaosgue/helloandroidstudio/ObjectOrientedProgramming/2-challengeClass.kt
+++ app/src/main/java/com/erikaosgue/helloandroidstudio/ObjectOrientedProgramming/2-challengeClass.kt
@@ -3,29 +3,74 @@ package com.erikaosgue.helloandroidstudio.ObjectOrientedProgramming
 
 /*
 Create a class representing a book. Every book has a title, an author, a
-publication year and is either borrowed or not. Choose an appropriate
-data type for each of these properties.
+publication year and is either borrowed or not.
+Choose an appropriate data type for each of these properties.
 A book can be borrowed from you or returned to you. In both cases, the
 borrowed property is updated accordingly. Also, a book should be able
 to print itself to the command line, including its title, author, and
 publication year.
+
+
+Compile the File:
+$ kotlinc 2-challengeClass.kt -include-runtime -d 2-challengeClass.jar
+
+Run the jar file:
+$ java -jar 2-challengeClass.jar
 */
 
-class Book(val title: String, val author: String, val publishYear: Int, var borrow: Boolean) {
+class Book(val title: String,
+           val author: String,
+           val publishYear: Int,
+           var borrow: Boolean) {
     init {
 
     }
+    fun borrowed() {
+        if (borrow) {
+            println("The book is already borrow")
+            return
+        }
+        else {
+            this.borrow = true
+            println("You have succefully borrowed the Book")
+            return
+        }
 
-    fun info() {
-        println("Title: ${this.title}\nAuthor: ${this.author}\nPublication Year: ${this.publishYear}\nIs Borrowed: ${this.borrow}")
     }
 
+    fun returned() {
+        if (borrow) {
+            println("Thanks for returning the Book")
+            this.borrow = false
+            return
+        }
+        else {
+            println("the book can't be returned")
+            return
+        }
+    }
+
+    fun info() {
+        val borrow = if (this.borrow == true) "Yes" else "No"
+        println("Title: ${this.title}\n" +
+                "Author: ${this.author}\n" +
+                "Publication Year: ${this.publishYear}\n" +
+                "Is Borrowed: ${borrow}")
+    }
 
 }
 
 fun main() {
-    var book = Book("100 años de Soledad", "Gabriel García Márquez", 1967, true)
+    var book = Book(
+            "100 años de Soledad",
+            "Gabriel García Márquez",
+            1967, true)
+
     book.info()
+    book.returned()
+    book.borrowed()
+    book.borrowed()
+    book.returned()
 }
 
 

e46a420899bfbd1bc084f0c414de2a5a7990dfb7
erikaosgue <1476@holbertonschool.com>
2021-02-22 21:29:20 -0500

    Add Color class as Enum exercise

---
+++ app/src/main/java/com/erikaosgue/helloandroidstudio/ObjectOrientedProgramming/Color.kt
@@ -0,0 +1,11 @@
+package com.erikaosgue.helloandroidstudio.ObjectOrientedProgramming
+
+// Enum color class
+// It is type Safety declaring the available values
+// (in this case the colors), this avoid
+// failing or performance issues,
+// because they are int representation internally
+// but we can still access the value Color as a string
+enum class Color {
+    RED, GREEN, BLUE
+}

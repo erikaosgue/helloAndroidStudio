9d4fba64cb95359aea61aa7d2f627c4ca1fe33ae
erikaosgue <1476@holbertonschool.com>
2021-02-22 21:24:38 -0500

    Update interfaces

--- app/src/main/java/com/erikaosgue/helloandroidstudio/ObjectOrientedProgramming/interfaces.kt
+++ app/src/main/java/com/erikaosgue/helloandroidstudio/ObjectOrientedProgramming/interfaces.kt
@@ -11,8 +11,16 @@ interface Buildable {
     fun build()
 }
 
+//This is for the old exercise
+//class Car(val color: String): Drivable, Buildable {
 
-class Car(val color: String): Drivable, Buildable {
+// the following line change String for Color class
+// This uses enum class which is better for the program
+// It is type Safety declaring the available values, this avoid
+// failing or performance issues, because they are int representation internally
+// but we cn still access the value Color as a string
+
+class Car(val color: Color): Drivable, Buildable {
     override val timeRequired = 120
 
     override fun drive() {
@@ -36,8 +44,13 @@ class MotorCycle(val color: String): Drivable {
 
 fun main(args: Array<String>) {
 
-    val car: Drivable = Car("Blue")
-    car.drive()
+//    val car: Drivable = Car("Blue")
+//    car.drive()
+
+    //The following is for the exercise with the
+    //color Class
+    val car = Car(Color.GREEN)
+    println(car.color)
 
     val motorCycle: Drivable = MotorCycle("red")
     motorCycle.drive()

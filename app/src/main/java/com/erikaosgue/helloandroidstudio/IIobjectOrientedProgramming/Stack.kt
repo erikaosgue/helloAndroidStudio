babbce86b6f3dcb8480b8bf2ea7e973c33f15410
erikaosgue <1476@holbertonschool.com>
2021-02-24 14:12:05 -0500

    Update the stack exercise for Generic class and Functions

--- app/src/main/java/com/erikaosgue/helloandroidstudio/IIobjectOrientedProgramming/Stack.kt
+++ app/src/main/java/com/erikaosgue/helloandroidstudio/IIobjectOrientedProgramming/Stack.kt
@@ -1,13 +1,15 @@
 package com.erikaosgue.helloandroidstudio.IIobjectOrientedProgramming
 
 /*
- * <E> Angles Brackets Parameters for
+ * <E> Angles Brackets for Parameters of type E
  * Generic classes
- * E => Can be any element:
+ * E => Can be any element type (Generic Type):
  * class, int, double, wherever type!
  */
 
 class Stack<E>(vararg val items: E) {
+
+    // items will be an Array
 
     val elements = items.toMutableList()
 
@@ -34,7 +36,33 @@ class Stack<E>(vararg val items: E) {
     }
 }
 
+
+/*
+ * Creating a Generic Function
+ * T -> will be type strings in the example stack2
+ * vararg elements will be an Array<T> -> Array<String>
+ * Return will be the call of the class Stack<T> -> Stack<String>("Hi", "Hey", "Hello")
+*/
+
+fun <T>stackOf(vararg elements: T): Stack<T> {
+    print("Elements in stackOf generic function will enter as an ")
+
+    // Checking the data type of the
+    println("${elements::class.simpleName}") //Array
+    // println("${elements::class.qualifiedName}") //kotlin.Array
+
+    // Printing the elements in the Array
+    //elements.forEach(::println)
+    // Return the instance of the generic class Stack
+    // use the start * to send the Array as vararg
+
+    return Stack(*elements)
+}
+
 fun main() {
+
+    println("\n****** stack1 ********")
+
     val stack = Stack(3,5, 3, 8)
     stack.print()
     println("Pop: ${stack.pop()}")
@@ -48,4 +76,11 @@ fun main() {
     stack.push(e)
     stack.print()
 
+    println("\n ****** stack2 ********")
+    val stack2 = stackOf("Hi", "Hey", "Hello")
+    stack2.print()
+
+    println("\n ****** stack3 ********")
+    val stack3 = stackOf("Hi")
+    stack3.print()
 }

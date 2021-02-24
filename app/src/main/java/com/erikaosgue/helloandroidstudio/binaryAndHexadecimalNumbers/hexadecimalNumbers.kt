2a30fadd4b9c43bedf4e28e412da047f90ac8209
erikaosgue <1476@holbertonschool.com>
2021-02-24 14:12:30 -0500

    Add binary and hexadecimal exercise

---
+++ app/src/main/java/com/erikaosgue/helloandroidstudio/binaryAndHexadecimalNumbers/hexadecimalNumbers.kt
@@ -0,0 +1,15 @@
+package com.erikaosgue.helloandroidstudio.binaryAndHexadecimalNumbers
+
+enum class Color {
+    RED, GREEN, BLUE
+}
+
+fun main(args: Array<String>) {
+    // * Decimal numbers from 0-9:
+    // 12540
+    // (1*10 ^4)+(2*10^3)+(5*10^2)+(4*10^1)+(0*10^0)
+
+    // * Hexadecimal from 0-9-A-F
+    // 10x12540 = (1*16 ^4)+(2*16^3)+(5*16^2)+(4*16^1)+(0*16^0) = 750072
+
+}

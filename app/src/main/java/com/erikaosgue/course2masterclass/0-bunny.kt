eb4c659ecc9918177e763ee44dd78dab853ec1d4
erikaosgue <1476@holbertonschool.com>
2021-02-25 12:58:37 -0500

    Add exercise about variables and string, Draw a bunny

---
+++ app/src/main/java/com/erikaosgue/course2masterclass/0-bunny.kt
@@ -0,0 +1,12 @@
+package com.erikaosgue.course2masterclass
+
+/** Lesson 23:
+ * Exercise: Variables and Strings
+ * Draw a bunny
+ */
+
+fun main(args: Array<String>) {
+
+    println("\n")
+    println("\t\t(\\(\\\n\t\t(-.-)\n\t\to_(\")(\")\n\n")
+}

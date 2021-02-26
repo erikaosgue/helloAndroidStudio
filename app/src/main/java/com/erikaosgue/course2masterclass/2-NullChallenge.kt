a0ad745e311a8ba97572f5616ed8e0c52ffe40ff
erikaosgue <1476@holbertonschool.com>
2021-02-26 16:24:46 -0500

    Update Null challenge with elvis Operator and not null assertion

--- app/src/main/java/com/erikaosgue/course2masterclass/2-NullChallenge.kt
+++ app/src/main/java/com/erikaosgue/course2masterclass/2-NullChallenge.kt
@@ -11,10 +11,20 @@ Print the length of that string to the console.
 
 fun main() {
 
+    /**** Safe Calls ****/
     print("Please enter a number: ")
     var number :Double? = readLine()?.toDouble()?.times(7)
 
     println("The length of the $number times 7 is: ${number?.toString()?.length}")
 
+    /**** Elvis Operator *****/
+    val nullStr :String? = null
+    println(nullStr?:"The string is null") // The string is null
+    println(nullStr?:"Elvis Operator".length) // 14
 
+
+    /**** Null assertion ****/
+
+    val nullAsertion :String? = null
+    println("This is a Null Asertion ${nullAsertion!!.length}") // 14
 }

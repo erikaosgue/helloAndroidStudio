f035ab33bbedb40a4ca3cb71221e0b4054b24a69
erikaosgue <1476@holbertonschool.com>
2021-02-26 17:45:36 -0500

    Add challenge description about exceptions

--- app/src/main/java/com/erikaosgue/course2masterclass/4-challengeTryAndCatch.kt
+++ app/src/main/java/com/erikaosgue/course2masterclass/4-challengeTryAndCatch.kt
@@ -3,7 +3,15 @@ package com.erikaosgue.course2masterclass
 import java.lang.Exception
 
 
-/* Lesson 90. Exercise try and Catch*/
+/* Lesson 90. Exercise try and Catch
+
+A customer will tell the program what product they want to buy.
+Then, they will tell the program how many products they require.
+Assume the price of the product is 9.99
+What is the total?
+Handle any exceptions that might occur.
+
+*/
 
 fun main(args: Array<String>) {
     println("What product do you want to buy? ")

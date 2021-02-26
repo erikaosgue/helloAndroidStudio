1de98a81bfbd7954c84dd4bb72e6f3ef7f174f4b
erikaosgue <1476@holbertonschool.com>
2021-02-26 11:45:34 -0500

    Update challenge operators

--- app/src/main/java/com/erikaosgue/course2masterclass/1-challengeOperators.kt
+++ app/src/main/java/com/erikaosgue/course2masterclass/1-challengeOperators.kt
@@ -7,7 +7,8 @@ fun main(args: Array<String>) {
     println("Enter the amount in your bank account: ")
     var input = readLine()?.toDouble()?:0.0
     println("The amount of money that you will have after 5 year with interest 5.5% is: ")
-    var totalAfter5Year = input + (5 *input*0.055)
+
+    var totalAfter5Year = input * (1 + (0.055 * 5))
     println(totalAfter5Year)
 
 }

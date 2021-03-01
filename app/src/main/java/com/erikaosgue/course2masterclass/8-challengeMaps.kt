8920aa3600fa2b4b8c4c51ca8352621ea3b7144c
erikaosgue <1476@holbertonschool.com>
2021-03-01 16:48:19 -0500

    Add challenge using Maps

--- app/src/main/java/com/erikaosgue/course2masterclass/8-challengeMaps.kt
+++ app/src/main/java/com/erikaosgue/course2masterclass/8-challengeMaps.kt
@@ -1,2 +1,28 @@
 package com.erikaosgue.course2masterclass
 
+/*
+
+You manage an amusement park, and you have a map
+that stores dates and attendance.
+var attendance = hashMapOf(Pair(“23 Sept”, 2837), Pair(“24 Sept”, 3726), Pair(“25 Sept”, 6253))
+Add a value for 26 Sept.
+How many people attended in total on 25 and 26 Sept?
+Is data for 22 Sept available?
+
+*/
+
+fun main() {
+
+    var attendance = hashMapOf(Pair("23 Sept", 2837), Pair("24 Sept", 3726), Pair("25 Sept", 6253))
+    attendance["26 Sept"] = 1122
+
+    /*** It is important to use the elvis Operator Because we may get a Null value ***/
+    var attend25 = attendance["25 Sept"]?:0
+    var attend26 = attendance["26 Sept"]?:0
+
+    println("Total people attending the 25 and 26 was: ${attend25 + attend26}")
+
+    println("Is data for the 22 Sep available: ${attendance.containsKey("22 Sept")}")
+
+
+}

72b48dcf654a4e743202bdba8853b24368416589
erikaosgue <1476@holbertonschool.com>
2021-03-02 16:57:34 -0500

    Add example of higher order function and Lambda

---
+++ app/src/main/java/com/erikaosgue/course2masterclass/13-HihgerOrderFunctionsLambda.kt
@@ -0,0 +1,23 @@
+package com.erikaosgue.course2masterclass
+
+fun main(args: Array<String>) {
+
+    /**** Option 1 Declaring parameters before******/
+   /* val names: ArrayList<String> = arrayListOf("Alica", "Dani", "Erika")
+    val myLambdaFun = {name: String -> println("Hello there $name")}
+    sayHello(names, myLambdaFun)
+    */
+
+    /**** Option 2 declaring the lambda function in the call******/
+    /*val names: ArrayList<String> = arrayListOf("Alica", "Dani", "Erika")
+    sayHello(names) {name: String -> println("Hello there $name")}
+    */
+
+    /**** Option 3 Declaring both parameters wothin the call of the function******/
+    sayHello(arrayListOf("Alica", "Dani", "Erika")) {name: String -> println("Hello there $name")}
+}
+
+fun sayHello(names: ArrayList<String>, myLambdaFun: (String) -> Unit) {
+    for (name in names)
+        myLambdaFun(name)
+}

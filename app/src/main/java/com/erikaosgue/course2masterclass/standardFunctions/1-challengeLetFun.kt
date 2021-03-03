589078e2dc4584bf31c0d0075044939bc481c458
erikaosgue <1476@holbertonschool.com>
2021-03-03 17:52:41 -0500

    Add first challenge using let function

--- app/src/main/java/com/erikaosgue/course2masterclass/standardFunctions/1-challengeLetFun.kt
+++ app/src/main/java/com/erikaosgue/course2masterclass/standardFunctions/1-challengeLetFun.kt
@@ -1,5 +1,12 @@
 package com.erikaosgue.course2masterclass.standardFunctions
 
+/*
+* Read client names from the console until the word stop is introduced.
+*  If the string is empty, the client is null.
+* Print out a hello message for each client.
+*/
+
+
 fun main() {
     val clients = arrayListOf<String?>()
 

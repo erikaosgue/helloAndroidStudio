ed770556780f4aedc42ca3ce6eaa6df6cc58569a
erikaosgue <1476@holbertonschool.com>
2021-03-03 17:51:38 -0500

    Add first challenge using let function

--- app/src/main/java/com/erikaosgue/course2masterclass/standardFunctions/1-challengeLetFun.kt
+++ app/src/main/java/com/erikaosgue/course2masterclass/standardFunctions/1-challengeLetFun.kt
@@ -1,2 +1,27 @@
 package com.erikaosgue.course2masterclass.standardFunctions
 
+fun main() {
+    val clients = arrayListOf<String?>()
+
+    do {
+        println("Enter client name: ")
+        val input = readLine()
+        input?.let {  // if input is != null will enter
+            if (it.toLowerCase() != "stop")
+            if (it == "")
+                clients.add(null)
+            else
+                clients.add(it)
+
+        }
+
+    }while(input?.toLowerCase() != "stop")
+
+    println(clients)
+
+    clients.forEach {
+        it?.let {
+            println("Hello $it")
+        }
+    }
+}

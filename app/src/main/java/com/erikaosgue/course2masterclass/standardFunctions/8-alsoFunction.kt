d0b3f2fe23e930c42753405d32f0fa1d64161434
erikaosgue <1476@holbertonschool.com>
2021-03-04 11:39:12 -0500

    Add Also function exercise

--- app/src/main/java/com/erikaosgue/course2masterclass/standardFunctions/8-alsoFunction.kt
+++ app/src/main/java/com/erikaosgue/course2masterclass/standardFunctions/8-alsoFunction.kt
@@ -1,8 +1,16 @@
 package com.erikaosgue.course2masterclass.standardFunctions
 
+/**
+ * 1. Apply with Car Object
+ * 2. Everytime a book is printed, company policy states that we must
+ * log the product and send an email
+ *
+ * Implement this functionality in a program
+ */
 
 fun main() {
 
+    println("===== 1. Car exercise ======\n")
     Car().apply {
         speed = 80
         drive()
@@ -11,6 +19,23 @@ fun main() {
             println("Car is running")
             println("Car speed is ${car.speed}")
         }
+
+    println("\n===== 2. Book exercise ======\n")
+    Book().apply {
+        printBook()
+    }.also { book ->
+        // Assume this will go to a log
+        println("Printing book $book")
+        println("Sending an email about book $book")
+
+    }
 }
 
 
+/*** 2 ***/
+class Book {
+    fun printBook() {
+        println("Printing a book")
+    }
+
+}
